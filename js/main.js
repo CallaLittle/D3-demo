05f4d077b18dc54e59257cff8d64a56b6f735b51
CallaLittle <Student@shc.wisc.edu>
2016-03-14 13:04:36 -0500

    Work on module 7
    Gather 4 city populations

--- js/main.js
+++ js/main.js
@@ -5,11 +5,56 @@ window.onload = function() {
 	var container = d3.select('body')
 		.append('svg')
 		.attr({class: 'container', width: w, height: h});
-	var rectange = container.append('rect')
+	var rectangle = container.append('rect')
 		.datum(400)
+		.attr('class', 'rectangle')
 		.attr('width', function(d) {
 			return d *2
 		})
-		.attr({class: 'rectangle', height: 250});
-	console.log(d);
+		.attr('height', function(d) {
+			return d; 
+		})
+		.attr('x', 50)
+		.attr('y', 50)
+		.style('fill', '#FFF');
+	
+	var cityPopulation = [
+
+		{
+			city: 'Nashville',
+			pop: 659042
+		},
+
+		{
+			city: 'Miami',
+			pop: 417650
+		},
+
+		{
+			city: 'Phoenix',
+			pop: 1513000
+		},
+
+		{
+			city: 'Atlanta',
+			pop: 447841
+		}
+		
+	];
+
+	var circles = container.selectAll('.circles')
+		.data(dataArray)
+		.enter()
+		.append('circle')
+		.attr('class', 'circles')
+		.attr('r', function(d, i) {
+			console.log(d + " " + i);
+			return d;
+		})
+		.attr('cx', function(d, i) {
+			return 70 + (i * 180);
+		})
+		.attr('cy', function(d) {
+			return 450 - (d * 5);
+		});
 };
